ea6d92dc6469694b5b5de49fda4770ed84015d77
Quinn Meurer <quinnyyy@bu.edu>
2019-02-10 17:39:00 -0500

    Changed main to be loop

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from gui import runUI
 from error_handler import Error_Handler
 from data import data_pull
+import time
 
-pdata = data_pull()
-print(pdata)
-is_warning, identifier = Error_Handler(pdata)
-print(is_warning)
-print(identifier)
+while True:
+    time.sleep(2)
+    pdata = data_pull()
+    print(pdata)
+    is_warning, identifier = Error_Handler(pdata)
+    print(is_warning)
+    print(identifier)
 #runUI()
